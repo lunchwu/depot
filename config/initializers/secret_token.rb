9861a5730f7cd3c4eab71046efd1eeec7cff085c
fwu <lunchwu@yeah.net>
2013-08-02 22:23:36 +0800

    Init commit, just create rails APP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'c60382a273ae4c01d8d6bfe6e04e097107fc0624233333fb72290db28bda5f54679e37f028241cad4d963154ca66c019bcfef9bf804af4de45cddcc99f2f7c22'
