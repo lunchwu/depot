0874c80bf4adc0fe111e37b70b265f28b971e0ff
fwu <lunchwu@yeah.net>
2013-08-10 14:32:46 +0800

    Add clear any carts feature
    Add clear any carts feature
    
    Signed-off-by: fwu <lunchwu@yeah.net>

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -49,8 +49,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
+	@current_item = @line_item
         format.html { redirect_to(root_url, :notice => 'Line item was successfully created.') }
-	format.js {@current_item = @line_item}
+	format.js
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
